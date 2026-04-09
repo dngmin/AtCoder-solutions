a23841b8eeb3c9a537eb6a7bb91de89f07980278
dngmin <kimdongmin2248@gmail.com>
2026-04-09 20:52:14 +0900

    solution ABC314-A in cpp

---
+++ ABC314/wcpp/A.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string pi = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+    int N;
+    std::cin >> N;
+    std::cout << pi.substr(0,2+N);
+    return 0;
+}
