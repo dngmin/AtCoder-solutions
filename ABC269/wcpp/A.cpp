fac3acba24789b7c878b9c7e396e150aca142e68
dngmin <kimdongmin2248@gmail.com>
2026-06-16 15:58:49 +0900

    solution ABC269-A in cpp

---
+++ ABC269/wcpp/A.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    int a, b, c, d; std::cin >> a >> b >> c >> d;
+    char Takahashi[] = "Takahashi";
+    std::cout << ((a+b) * (c-d)) << "\n" << Takahashi;
+    return 0;
+}
