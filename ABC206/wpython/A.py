ce5ac3c5827b3473bd169d2ea9b0d515f95be891
dngmin <kimdongmin2248@gmail.com>
2026-08-12 20:18:11 +0900

    solution ABC206-A,B

---
+++ ABC206/wpython/A.py
@@ -0,0 +1,2 @@
+price = (1.08 * int(input())) // 1
+print("Yay!" if price < 206 else ":(" if price > 206 else "so-so")
---
+++ ABC206/wpython/B.py
@@ -0,0 +1,4 @@
+N = int(input())
+i = 0
+while i * (i + 1) // 2 < N: i += 1
+print(i)
